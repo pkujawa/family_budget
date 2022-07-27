5bef75a8cf79e4c8c9064c9aee5447a20e4fdba8
pkujawa <p.kujawa2@op.pl>
2022-07-27 14:15:45 +0200

    add models, category choices

---
+++ exercise/family_budget/choices.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class CategoryChoices(models.TextChoices):
+    HOUSING = 'HOUSING', _('Housing')
+    TRANSPORTATION = 'TRANSPORTATION', _('Transportation')
+    FOOD = 'FOOD', _('Food')
+    MEDICAL_AND_HEALTHCARE = 'MEDICAL_AND_HEALTHCARE', _('Medical & Healthcare')
+    UTILITIES = 'UTILITIES', _('Utilities')
+    SAVING_AND_DEBT_PAYMENTS = 'SAVING_AND_DEBT_PAYMENTS', _('Saving & Debt Payments')
+    INSURANCE = 'INSURANCE', _('Insurance')
+    WORK = 'WORK', _('Work')
+    OTHER = 'OTHER', _('Other')
--- exercise/family_budget/models.py
+++ exercise/family_budget/models.py
@@ -1,4 +1,20 @@
 from django.db import models
+from .choices import CategoryChoices
 
 
+class IncomeExpense(models.Model):
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(choices=CategoryChoices.choices, default=CategoryChoices.OTHER, max_length=100)
 
+
+class Income(IncomeExpense):
+    budget = models.ForeignKey('Budget', related_name='incomes', on_delete=models.CASCADE)
+
+
+class Expense(IncomeExpense):
+    budget = models.ForeignKey('Budget', related_name='expenses', on_delete=models.CASCADE)
+
+
+class Budget(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='my_budgets', on_delete=models.CASCADE)
+    shared_with = models.ManyToManyField('auth.User', related_name='shared_budgets')
